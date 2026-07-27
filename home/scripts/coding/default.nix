b8741973981a89bb02fddb563acd25f958ed850c
Star <252556693+StarTheSus@users.noreply.github.com>
2026-07-27 17:58:58 +0300

    refactor(home): remove commented out sections in coding scripts

--- home/scripts/coding/default.nix
+++ home/scripts/coding/default.nix
@@ -4,14 +4,7 @@
   home.packages = with pkgs; [
     (writeShellScriptBin "flakenv" (builtins.readFile ./flakenv.sh))
     (writeShellScriptBin "stargit" (builtins.readFile ./stargit.sh))
-
     # Future coding scripts go here:
     # (writeShellScriptBin "build-runner" (builtins.readFile ./build-runner.sh))
   ];
-  # home.packages = [
-  #   (import ./flakenv.nix { inherit pkgs; })
-  #   (import ./stargit.nix { inherit pkgs; })
-  #   # Future coding scripts go here:
-  #   # (import ./build-runner.nix { inherit pkgs; })
-  # ];
 }
