98f1fb9b2bf42d6c85f05d35b306076bc0a4cf2b
Star <252556693+StarTheSus@users.noreply.github.com>
2026-08-07 01:41:06 +0300

    chore(modules): reformat desktop/xdg.nix

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, ... }:
 
 {
-#
+  #
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome 
+      xdg-desktop-portal-gnome
     ];
     # Force the system to actually use them
     # config.common.default = "*";
@@ -14,7 +14,7 @@
       niri = {
         # Use GTK by default for all basic dialogs (like the file picker)
         default = lib.mkForce [ "gtk" ];
-        
+
         # Explicitly route screen sharing and casting to the GNOME portal
         "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
         "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
