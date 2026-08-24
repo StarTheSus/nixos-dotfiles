75507ec0b267f7d9802ad67650d4b5d99796fa61
Star <252556693+StarTheSus@users.noreply.github.com>
2026-08-24 12:28:30 +0300

    feat(modules/system): add plymouth and re-organize bootloader.nix

--- modules/system/bootloader.nix
+++ modules/system/bootloader.nix
@@ -1,13 +1,14 @@
 { pkgs, inputs, ... }:
 
 {
-  boot.loader = {
-    # Shared
-    efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      # Shared
+      efi.canTouchEfiVariables = true;
 
-    # Systemd-boot
-    # systemd-boot.configurationLimit = 20;
-    # systemd-boot.enable = true;
+      # Systemd-boot
+      # systemd-boot.configurationLimit = 20;
+      # systemd-boot.enable = true;
 
       # Grub
       systemd-boot.enable = false;
@@ -27,14 +28,18 @@
             halt
           }
 
-        menuentry "UEFI Firmware Settings" --class uefi {
-          fwsetup
-        }
+          menuentry "UEFI Firmware Settings" --class uefi {
+            fwsetup
+          }
 
-        menuentry "Memory Tester (memtest86+)" --class memtest {
-        chainloader /memtest.efi
-        }
-      '';
+          menuentry "Memory Tester (memtest86+)" --class memtest {
+          chainloader /memtest.efi
+          }
+        '';
+      };
+    };
+    plymouth = {
+      enable = true;
     };
   };
 }
