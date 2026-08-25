ba1665c389910ab70a44eebf6e4caf5e78bdd08e
Star <252556693+StarTheSus@users.noreply.github.com>
2026-08-25 09:18:19 +0300

    fix(modules/desktop): update xdg.nix to define certain mimes declaratively
    Since antigravity feels like being fucking funny

--- modules/desktop/xdg.nix
+++ modules/desktop/xdg.nix
@@ -7,10 +7,29 @@
     xdg-user-dirs
   ];
 
-        # Explicitly route screen sharing and casting to the GNOME portal
-        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+      # Force the system to actually use them
+      # config.common.default = "*";
+      config = {
+        niri = {
+          # Use GTK by default for all basic dialogs (like the file picker)
+          default = lib.mkForce [ "gtk" ];
+
+          # Explicitly route screen sharing and casting to the GNOME portal ffs
+          "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+          "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+        };
       };
+    };
+    mime.defaultApplications = {
+      "inode/directory" = [ "yazi.desktop" ];
+      "x-scheme-handler/terminal" = [ "kitty.desktop" ];
     };
   };
 }
