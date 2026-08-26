c1794963422347d426b238787413f08b17441c66
Star <252556693+StarTheSus@users.noreply.github.com>
2026-08-26 20:57:13 +0300

    chore(home): add config and default.nix imports

--- home/default.nix
+++ home/default.nix
@@ -1,28 +1,29 @@
 { config, pkgs, ... }:
 
 {
-    imports = [
-        ./services
-        ./scripts
-    ];
+  imports = [
+    ./services
+    ./scripts
+    ./config
+  ];
 
-    programs.home-manager.enable = true;
-    
-    home = {
-        username = "star";
-        homeDirectory = "/home/star";
-        stateVersion = "26.05"; 
-        preferXdgDirectories = true;
-        enableNixpkgsReleaseCheck = true;
-        # file = {
-        #     ".local/bin" = {
-        #         source = ./files/bin;
-        #         recursive = true;
-        #     };
-        #     ".local/share/menus" = {
-        #         source = ./files/menus;
-        #         recursive = true;
-        #     };
-        # };
-    };
+  programs.home-manager.enable = true;
+
+  home = {
+    username = "star";
+    homeDirectory = "/home/star";
+    stateVersion = "26.05";
+    preferXdgDirectories = true;
+    enableNixpkgsReleaseCheck = true;
+    # file = {
+    #     ".local/bin" = {
+    #         source = ./files/bin;
+    #         recursive = true;
+    #     };
+    #     ".local/share/menus" = {
+    #         source = ./files/menus;
+    #         recursive = true;
+    #     };
+    # };
+  };
 }
