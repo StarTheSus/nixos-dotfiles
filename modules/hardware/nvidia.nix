b32f32f7932d2bcda09fe0e589d65d3558ae2d62
Star <252556693+StarTheSus@users.noreply.github.com>
2026-09-02 12:31:52 +0300

    feat(modules/hardware): enable modesetting to let nvidia enter d3cold
    Apparently, an update changed novideo's behavior, modesetting and power
    management alone isn't enough.
    The [wiki](https://wiki.nixos.org/wiki/NVIDIA) however specifies how to
    enable this behavior.

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -11,24 +11,36 @@
   };
 
   # use novideo's driver
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [
+    "modesetting" # For offloading, to allow nvidia to sleep
+    "nvidia"
+  ];
 
   hardware.nvidia = {
     # Options
     modesetting.enable = true;
-	powerManagement.enable = true;
+    powerManagement.enable = true;
     # I SAAAAAAAAAID, GET OUUUUUUUUUUUUUUUT
     open = false;
     # Nvidia Settings Menu
     nvidiaSettings = true;
     # Prod, stable in theory
     package = config.boot.kernelPackages.nvidiaPackages.production;
+
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0@0:2:0";
+      nvidiaBusId = "PCI:1@0:0:0";
+    };
   };
 
   # Nix doesn't wrap these... for some reason
   boot.extraModprobeConfig = ''
-    options nvidia NVreg_TemporaryFilePath=/var/tmp
-	options nvidia NVreg_PreserveVideoMemoryAllocations=1
-    # options nvidia NVreg_DynamicPowerManagement=0x03
+        options nvidia NVreg_TemporaryFilePath=/var/tmp
+    	options nvidia NVreg_PreserveVideoMemoryAllocations=1
+        # options nvidia NVreg_DynamicPowerManagement=0x03
   '';
 }
